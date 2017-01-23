c692e2db0d6eb30b8b619305870a168c3a4919cd
michelleremmett <G00181616@stu.raritanval.edu>
2017-01-23 12:06:35 -0500

    Add files via upload

--- Picture.java
+++ Picture.java
@@ -1,20 +1,18 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ * This is supposed to be a house with a window on the roof and in the middle. The squares are a door.
  *
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2011.07.31
+ * @author  Michelle Remmett
+ * @version 2017.01.23
  */
 public class Picture
 {
     private Square wall;
     private Square window;
+    private Square window2;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
+    private Person person; 
 
     /**
      * Constructor for objects of class Picture
@@ -37,23 +35,67 @@ public class Picture
         
         window = new Square();
         window.changeColor("black");
-        window.moveHorizontal(-120);
-        window.moveVertical(40);
-        window.changeSize(40);
+        window.moveHorizontal(-95);
+        window.moveVertical(110);
+        window.changeSize(30);
         window.makeVisible();
+        
+        window2 = new Square();
+        window2.changeColor("black");
+        window2.moveHorizontal(-95);
+        window2.moveVertical(90);
+        window2.changeSize(30);
+        window2.makeVisible();
 
         roof = new Triangle();  
         roof.changeSize(60, 180);
         roof.moveHorizontal(20);
         roof.moveVertical(-60);
         roof.makeVisible();
-
+        
         sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(100);
-        sun.moveVertical(-40);
-        sun.changeSize(80);
+        sun.changeColor("white");
+        sun.moveHorizontal(-15);
+        sun.moveVertical(10);
+        sun.changeSize(30);
         sun.makeVisible();
+
+        sun2 = new Circle();
+        sun2.changeColor("white");
+        sun2.moveHorizontal(-15);
+        sun2.moveVertical(70);
+        sun2.changeSize(30);
+        sun2.makeVisible();
+        
+        person = new Person ();
+        person.changeColor("black");
+        person.moveHorizontal(60);
+        person.moveVertical(30);
+        person.makeVisible();
+        
+        person = new Person ();
+        person.changeColor("black");
+        person.moveHorizontal(90);
+        person.moveVertical(30);
+        person.makeVisible();
+        
+        person = new Person ();
+        person.changeColor("black");
+        person.moveHorizontal(-150);
+        person.moveVertical(30);
+        person.makeVisible();
+        
+        person = new Person ();
+        person.changeColor("black");
+        person.moveHorizontal(-200);
+        person.moveVertical(30);
+        person.makeVisible();
+        
+        person = new Person ();
+        person.changeColor("black");
+        person.moveHorizontal(-250);
+        person.moveVertical(30);
+        person.makeVisible();
     }
 
     /**
@@ -65,8 +107,10 @@ public class Picture
         {
             wall.changeColor("black");
             window.changeColor("white");
+            window2.changeColor("white");
             roof.changeColor("black");
-            sun.changeColor("black");
+            sun.changeColor("white");
+            sun2.changeColor("white");
         }
     }
 
